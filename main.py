5830304e019aa92d0de9899686e921c12492aa22
kgazz <73496427+kgazz@users.noreply.github.com>
2022-09-26 17:30:29 -0300

    new commit

--- main.py
+++ main.py
@@ -1,141 +1,159 @@
 import pyglet
 from pyglet.window import key
+from pyglet.window import mouse
 from pyglet import shapes
 
-# create a batch class
-batch = pyglet.graphics.Batch()
-
-# Window
-width = 800
+# CRIANDO A JANELA E AS ESPECS
+width = 1000
 height = 600
-window = pyglet.window.Window(width, height, "One Player Ping Pong", resizable=False)
-window.set_caption("One Player Ping Pong")
+window = pyglet.window.Window(width, height, 'PING-PONG', resizable=False)
+window.set_caption('PING-PONG')
 
-# Key Handler
+# LIDAR COM O USO DO TECLADO
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
-# create a walls class
+# TENTANDO IMPLEMENTAR O MOUSE
+mouse = mouse.MouseStateHandler()
+window.push_handlers(mouse)
+
+# CRIANDO UM BATCH P RENDER GRAFICO
+batch = pyglet.graphics.Batch()
+
+# MARGEM DA PAREDE
 walls_padding = 10
-class Walls:
-    def __init__(self):
-        self.wall_left = shapes.Line(0, 0, 0, height, walls_padding, color=(255, 255, 255), batch=batch)
-        self.wall_right =  shapes.Line(width, 0, width, height, walls_padding, color=(255, 255, 255), batch=batch)
-        self.wall_top = shapes.Line(0, height, width, height, walls_padding, color=(255, 255, 255), batch=batch)
-        self.wall_bottom = shapes.Line(0, 0, width, 0, walls_padding, color=(255, 255, 255), batch=batch)
 
 
-    def draw(self):
-        self.wall_left.draw()
-        self.wall_right.draw()
-        self.wall_top.draw()
-        self.wall_bottom.draw()
+# DEFININDO PAREDES PARA O JOGO
+class walls:
+    def __init__(gameMenu):
+        gameMenu.wall_left = shapes.Line(0, 0, 0, height, walls_padding, color=(255, 255, 255), batch=batch)
+        gameMenu.wall_right = shapes.Line(width, 0, width, height, walls_padding, color=(255, 255, 255), batch=batch)
+        gameMenu.wall_top = shapes.Line(0, height, width, height, walls_padding, color=(255, 255, 255), batch=batch)
 
-# create a walls object
-walls = Walls()
+    def draw(gameMenu):
+        gameMenu.wall_left.draw()
+        gameMenu.wall_right.draw()
+        gameMenu.wall_top.draw()
 
-# create a paddle class
-class Paddle:
-    def __init__(self, x, y, width, height):
-        self.paddle = pyglet.shapes.Rectangle(x, y, width, height, color = (255, 0, 0), batch = batch)
 
-    def draw(self):
-        self.paddle.draw()
+# CRIAR UM OBJETO DA CLASSE 'PAREDE'
+walls = walls()
 
-    def update(self):
+
+# DEFININDO A 'RAQUETE'
+class paddle:
+    def __init__(gameMenu, x, y, width, height):
+        gameMenu.paddle = pyglet.shapes.Rectangle(x, y, width, height, color=(255, 255, 255), batch=batch)
+
+    def draw(gameMenu):
+        gameMenu.paddle.draw()
+
+    def update(gameMenu):
         addPosition = 100
-        print(self.paddle.x)
+        if keys[key.RIGHT] or keys[key.D] and gameMenu.paddle.x + addPosition < width - 10:
+            gameMenu.paddle.x += addPosition
+        elif keys[key.LEFT] or keys[key.A] and gameMenu.paddle.x - addPosition > -10:
+            gameMenu.paddle.x -= addPosition
 
-        if keys[key.RIGHT] and self.paddle.x + addPosition < width - 10:
-            self.paddle.x += addPosition
-        elif keys[key.LEFT] and self.paddle.x - addPosition > -10:
-            self.paddle.x -= addPosition
-        
 
-# create a paddle object
-paddle = Paddle(360, 20, 100 - walls_padding, 10)
+# CRIANDO UM OBJETO DA CLASSE PADDLE
+paddle = paddle(450, 5, 100, 10)
 
-# create a ball class
+
+# DEFININDO A CLASSE BOLA
 class Ball:
-    def __init__(self, x, y):
-        self.ball = pyglet.shapes.Circle(x, y, 10, color = (255, 255, 255), batch = batch)
+    def __init__(gameMenu, x, y):
+        gameMenu.ball = pyglet.shapes.Circle(x, y, 10, color=(255, 255, 255), batch=batch)
 
-    def draw(self):
-        self.ball.draw()
+    def draw(gameMenu):
+        gameMenu.ball.draw()
 
-    def update(self):
+    def update(gameMenu):
         addPosition = 100
 
-        # create ball movement
-        if keys[key.LEFT]:
-            self.ball.x -= addPosition + pyglet.clock.tick()
-        if keys[key.RIGHT]:
-            self.ball.x += addPosition + pyglet.clock.tick()
 
-
-# create a ball object
+# CRIANDO UM OBJETO BOLA
 ball = Ball(width / 2, height - 40)
-    
-# create a start menu class
+
+
+# CRIANDO A CLASSE DE MENU INICIAL
 class StartMenu:
-    def __init__(self):
-        self.menu = pyglet.text.Label("One Player Ping Pong", font_name="Arial", font_size=36, x=window.width / 2, y=window.height / 2, anchor_x="center", anchor_y="center")
-        self.start = pyglet.text.Label("Iniciar", font_name="Arial", font_size=24, x = window.width / 2, y=window.height/ 2-80, anchor_x="center", anchor_y="center")
-        self.exit = pyglet.text.Label("Sair", font_name="Arial", font_size=24, x = window.width / 2, y=window.height / 2- 130, anchor_x="center", anchor_y="center")
+    def __init__(StartMenu):
+        StartMenu.menu = pyglet.text.Label("One Player Ping Pong", font_name="Arial", font_size=36, x=window.width / 2,
+                                           y=window.height / 2 + 230, anchor_x="center", anchor_y="center")
 
-        self.selected = 0
-        self.menu_items = [self.start, self.exit]
-        self.menu_items[self.selected].color = (255, 0, 0, 255)
+    def draw(StartMenu):
+        StartMenu.menu.draw()
 
-    def draw(self):
-        self.menu.draw()
-        self.start.draw()
-        self.exit.draw()
 
-    def update(self):
-        if keys[key.UP]:
-            self.menu_items[self.selected].color = (255, 255, 255, 255)
-            self.selected -= 1
-            if self.selected < 0:
-                self.selected = len(self.menu_items)-1
-            self.menu_items[self.selected].color = (255, 0, 0, 255)
-
-        if keys[key.DOWN]:
-            self.menu_items[self.selected].color = (255, 255, 255, 255)
-            self.selected += 1
-            if self.selected > len(self.menu_items)-1:
-                self.selected = 0
-            self.menu_items[self.selected].color = (255, 0, 0, 255)
-
-        if keys[key.ENTER]:
-            if self.selected == 0:
-                self.menu.visible = False
-                self.start.visible = False
-                self.exit.visible = False
-                
-            if self.selected == 1:
-                pyglet.app.exit()
-
-# create a start menu object
+# CRIAR UM OBJETO MENU
 start_menu = StartMenu()
 
-# on draw function
+
+# DEFININDO BOTOES
+class Button:
+    def __init__(button):
+        button.start = pyglet.text.Label("Iniciar", font_name="Arial", font_size=24, x=window.width / 2,
+                                         y=window.height / 2 - 80, anchor_x="center", anchor_y="center")
+        button.exit = pyglet.text.Label("Sair", font_name="Arial", font_size=24, x=window.width / 2,
+                                        y=window.height / 2 - 130, anchor_x="center", anchor_y="center")
+
+        button.selected = 0
+        button.menu_items = [button.start, button.exit]
+        button.menu_items[button.selected].color = (255, 0, 0, 255)
+
+    def draw(button):
+        button.start.draw()
+        button.exit.draw()
+
+
+botao = Button()
+
+
+# DEFININDO A CLASSE SCORE BOX
+class score:
+    def __init__(gameMenu, x, y, width, height):
+        cont = 0
+        gameMenu.score = pyglet.shapes.Rectangle(x, y, width, height, color=(0, 0, 0), batch=batch)
+        gameMenu.scores = pyglet.text.Label("Score: {}".format(cont), font_name="Arial", font_size=15, x=50,
+                                            y=580, anchor_x="center", anchor_y="center")
+
+    def draw(gameMenu):
+        gameMenu.score.draw()
+        gameMenu.scores.draw()
+
+    def update(gameMenu):
+        addPosition = 0
+
+
+# CRIANDO UM OBJETO DA CLASSE SCORE
+score = score(5, 535, 150, 60)
+
+
 @window.event
 def on_draw():
     window.clear()
-
     start_menu.draw()
     walls.draw()
     paddle.draw()
     ball.draw()
+    score.draw()
+    botao.draw()
+
 
 def update(dt):
-    if  start_menu.menu.visible == True:
-        start_menu.update()
-    else: 
+    if start_menu.menu.visible == True:
+        pass
+    else:
         ball.update()
         paddle.update()
-       
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    print('clicou')
+
 
 if __name__ == '__main__':
     if start_menu.menu.visible == True:
@@ -144,4 +162,11 @@ if __name__ == '__main__':
         pyglet.clock.schedule_interval(update, 1 / 60)
 
     pyglet.app.run()
+
+
+
+
+
+
+
 
