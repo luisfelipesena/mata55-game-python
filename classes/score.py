914bf72e1519e720b674d9bea07999a2b9f42366
Luis Felipe Cordeiro Sena <61807189+luisfelipesena@users.noreply.github.com>
2022-09-26 23:38:43 -0300

    Update score.py

--- classes/score.py
+++ classes/score.py
@@ -1,23 +1,20 @@
 import pyglet
 
 class Score:
-    def __init__(self, x, y, width, height):
-        self.score = pyglet.shapes.Rectangle(x, y, width, height, color=(0, 0, 0))
-        self.score.visible = False
+    def __init__(self, x, y):   
+        self._score = pyglet.text.Label("Score: {}".format(0), font_name="Arial", font_size=15, x=x, y=y, anchor_x="center", anchor_y="center")
         self.score_count = 0;
-        self.score_label = pyglet.text.Label("Score: {}".format(self.score_count), font_name="Arial", font_size=15, x=50, y=580, anchor_x="center", anchor_y="center")
-
-
+        self._score.visible = False  
+      
     def draw(self):
-        self.score.draw()
-        self.score_label.draw()
+        self._score.draw()
 
     @property
     def visible(self):
-      return self._paddle.visible
+        return self._score.visible
     @visible.setter
     def visible(self, bool):
-        self._paddle.visible = bool
+        self._score.visible = bool
 
     def update(self):
         pass
